9dad4e8f53a92bd3bcfbd14e822607b363664a00
BTSSIO <BTSSIO@WIN10EDU>
2023-10-30 07:57:32 +0100

    Ajoutez des fichiers projet.

---
+++ B1.2 exercice 18/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B1._2_exercice_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Saisie des 3 nombres
+         
+            Console.WriteLine("Veuillez saisir le nombre 1 : ");
+            double nb1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez saisir le nombre 2 : ");
+            double nb2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Veuillez saisir le nombre 3 : ");
+            double nb3 = double.Parse(Console.ReadLine());
+
+            // Affichage du plus petit des trois
+            Console.WriteLine("Le plus petit nombre est : " + Math.Min(nb1, Math.Min(nb2, nb3)) + ".");
+            Console.ReadLine();
+        }
+    }
+}
